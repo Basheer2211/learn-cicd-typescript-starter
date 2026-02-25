aa8617b58aeeacd937406f7ba8759ddab5fdf482
unknown <basheirdaoud@gmail.com>
2026-02-25 02:51:18 +0200

    Fail CI on lint warnings

--- eslint.config.js
+++ eslint.config.js
@@ -2,25 +2,18 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
-    // Ignore generated folders ?
-    {
-        ignores: ["dist/**", "coverage/**", "node_modules/**"],
-    },
-
-    // JavaScript + TypeScript rules
     {
         files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-        languageOptions: {
-            globals: {
-                ...globals.node,
-            },
-        },
-        rules: {
-            ...js.configs.recommended.rules,
-        },
+        plugins: { js },
+        extends: ["js/recommended"],
     },
-
-    ...tseslint.configs.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+        languageOptions: { globals: globals.node },
+    },
+    tseslint.configs.recommended,
+    pluginSecurity.configs.recommended,
 ]);
